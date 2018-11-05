b2288ddb85ef7ba003fe9cc3f69e8c01157ed7ef
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-11-05 17:32:09 -0600

    Login/registration UI created; dashboard layout made

---
+++ ToDoList/ToDoList/Dashboard.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for dashboard.xaml
+    /// </summary>
+    public partial class dashboard : Page
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void createList(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- ToDoList/ToDoList/Login.xaml.cs
+++ ToDoList/ToDoList/Login.xaml.cs
@@ -37,22 +37,51 @@ namespace ToDoList
         }
         private void SignUpClick(object sender, RoutedEventArgs e)
         {
+            //change top button styling
             signUpButton.Background = mintGreen;
             signUpText.Foreground = Brushes.White;
 
             logInButton.Background = grey;
             logInText.Foreground = textGrey;
 
+            //change user/pass fields
+            usernameTextBox.Text = "Create Username";
+            passwordTextBox.Text = "Create Password";
+            passwordRetypeTextBox.Visibility = Visibility.Visible;
+
+            //change submit button
+            submitButtonText.Text = "Create User";
+            Grid.SetRow(submitButton, 10);
+
         }
         private void LogInClick(object sender, RoutedEventArgs e)
         {
+            //change top button styling
             logInButton.Background = mintGreen;
             logInText.Foreground = Brushes.White;
 
             signUpButton.Background = grey;
             signUpText.Foreground = textGrey;
 
+            //change user/pass fields
+            usernameTextBox.Text = "Enter Username";
+            passwordTextBox.Text = "Enter Password";
+            passwordRetypeTextBox.Visibility = Visibility.Hidden;
+
+            //change submit button
+            submitButtonText.Text = "Log In";
+            Grid.SetRow(submitButton, 8);
+
         }
 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void submitButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new Uri("Dashboard.xaml", UriKind.RelativeOrAbsolute));
+        }
     }
 }

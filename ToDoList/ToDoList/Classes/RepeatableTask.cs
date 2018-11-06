817f39c1006a60efb3938663dfebc128a7c94c0d
King, Cal <kingc51@uwosh.edu>
2018-11-06 17:53:44 -0600

    RepeatTaskAdded

---
+++ ToDoList/ToDoList/Classes/RepeatableTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class RepeatableTask
+    {
+        List<DisposableTask> copiesOfRepeatableTask;
+
+        RepeatableTask(DateTime repeatTaskDate, )
+        {
+
+        }
+        public string title { get; set; }
+
+        public string isComplete { get; set; }
+
+        public string allowNotifications { get; set; }
+
+        public string notes { get; set; }
+
+
+    }
+}

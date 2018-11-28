9f161284aa5faafd738a9135b2288cab21ca72aa
David Grunsted <grunsd41@uwosh.edu>
2018-11-28 09:21:55 -0600

    took connection string out

--- ToDoList/ToDoList/Classes/ToDoUser.cs
+++ ToDoList/ToDoList/Classes/ToDoUser.cs
@@ -40,6 +40,7 @@ namespace ToDoList
         public List<TaskList> getUserToDoList(){
             return UserToDoList;
         }
+
         public void SaveUser()
         {
             foreach (var task in UserToDoList[0].getTaskListList())

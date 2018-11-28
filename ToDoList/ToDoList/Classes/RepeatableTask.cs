2cadcdc451df2f80f22aced66a900699f1281e96
King, Cal <kingc51@uwosh.edu>
2018-11-27 18:43:23 -0600

    new repeatable

--- ToDoList/ToDoList/Classes/RepeatableTask.cs
+++ ToDoList/ToDoList/Classes/RepeatableTask.cs
@@ -10,37 +10,26 @@ namespace ToDoList
     
     class RepeatableTask : Task
     {
-        List<DisposableTask> copiesOfRepeatableTask;
-        // map of subtasks
-        //List<SubTask> listOfSubtasks;
         DateTime firstEvent;
         DateTime repeatOccurance;
+        protected Dictionary<int, SubTask> subTasks { get; set; } //subTaskId maps to subTask
 
-
-        RepeatableTask(DateTime firstOccurance, DateTime repeatOccur, String newTitle, Boolean allowNotif, String description, List<DisposableTask> copiesOfRepeatable)
+        RepeatableTask(String newTitle, String description, Boolean allowNotif, Boolean isComplete, DateTime taskDueDate)
         {
 
             taskTitle = newTitle;
             complete = false;
             notificationsOn = allowNotif;
             descrip = description;
-            firstEvent = firstOccurance;
-            repeatOccurance = repeatOccur;
-            copiesOfRepeatableTask = copiesOfRepeatable; // copiesOfRepeatable will be null if this is a new task
-            if(copiesOfRepeatable == null)
-            {
-                // have to call, create new list of disposable tasks
-            }
+            firstEvent = taskDueDate;
+ 
+            // copiesOfRepeatable will be null if this is a new task
+
             // need to input list of disposable tasks as well
             // need to then store new repeatable task in DB
             // also need to create repeatable list of disposable tasks.
         }
-       
-        private void createListOfFutureTasks()
-        {
-            // will have to go through and create copies, 30 or so future tasks and put them in list.
-        }
-        
+             
         public override void AddSubtask(int  subTaskId)
         {
 
@@ -68,60 +57,31 @@ namespace ToDoList
         public override void setTitle(String theTitle)
         {
             this.taskTitle = theTitle;
-            foreach (DisposableTask current in copiesOfRepeatableTask)
-            {
-                if (current.getIsComplete() == false)
-                {
-                    current.setTitle(theTitle);
-                }
-            }
         }
 
         public override void setIsComplete(Boolean complete)
         {
-
+            foreach( KeyValuePair<int, SubTask> entry in subTasks)
+            {
+               // entry.Value.dueDate = entry.Value.dueDate.AddDays(7);
+            }
+            
         }
 
         public override void setAllowNotifications(Boolean notifications)
         {
             this.notificationsOn = notifications;
-            foreach(DisposableTask current in copiesOfRepeatableTask)
-            {
-                if (current.getIsComplete() == false)
-                {
-                    current.setAllowNotifications(notifications);
-                }
-            }
         }
 
 
         public override void setDescription(String descri)
         {
             this.descrip = descri;
-            foreach (DisposableTask current in copiesOfRepeatableTask)
-            {
-                if (current.getIsComplete() == false)
-                {
-                    current.setDescription(descrip);
-                }
-            }
         }
 
         public override void setTaskFKey(int taskFKey)
         {
             
-        }
-
-        public override void setTaskDueDate(DateTime taskDueDate)
-        {
-            this.taskDueDate = taskDueDate;
-            foreach (DisposableTask current in copiesOfRepeatableTask)
-            {
-                if (current.getIsComplete() == false)
-                {
-                   // current.setTaskDueDate(taskDueDate); // do not want to set all disposable tasks to same date, want them offset
-                }
-            }
         }
 
         public override int getTaskId()
@@ -147,11 +107,6 @@ namespace ToDoList
         public override String getDescription()
         {
             return this.descrip;
-        }
-
-        public override DateTime getTaskDueDate()
-        {
-            return this.taskDueDate;
         }
 
         public override int getTaskFKey()

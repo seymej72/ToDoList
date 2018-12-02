f8777d9f01a6f22c5698076491c522f5b4570d71
Bostwick, Blake <bostwb66@uwosh.edu>
2018-12-02 12:52:41 -0600

    Implemented and tested tests for ToDo (hashing password and constructing a new user)

---
+++ ToDoList/ToDoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToDoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToDoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c85c0e85-af27-420f-9eeb-3d9cafe9ca90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ToDoList/ToDoTests/ToDoTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList;
+
+namespace ToDoTests
+{
+    [TestClass]
+    public class ToDoTests
+    {
+
+        #region ToDo class Tests
+        [TestMethod]
+        public void TestHashing()
+        {
+            var ToDoObj = new ToDo();
+
+            string pass = "1325465465";
+
+            int potentialPassword = ToDoObj.HashPassword(pass);
+
+            Assert.AreEqual(51082944, potentialPassword);
+        }
+
+        [TestMethod]
+        public void TestCreatingUser()
+        {
+            var ToDoObj = new ToDo();
+
+            var ActualUser = new ToDoUser("TestCase", 51082944);
+
+            var PotentialUser = ToDoObj.RegisterUser("TestCase", "1325465465");
+
+            Assert.AreEqual(ActualUser, PotentialUser);
+        }
+
+        #endregion
+    }
+}
